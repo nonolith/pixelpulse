51bc13fa21da3116043c3a3dd8af7b5ab95a884f
Kevin Mehall <km@kevinmehall.net>
2011-03-26 15:36:01 -0400

    data from input socket

--- dataserver.js
+++ dataserver.js
@@ -2,6 +2,7 @@ var http = require('http'),
     io = require('socket.io')
     paperboy = require('paperboy')
     path = require('path')
+    net = require('net')
     
 WEBROOT = path.dirname(__filename);
 
@@ -14,8 +15,19 @@ var socket = io.listen(server);
 
 var time = 0, acc=0;
 
-setInterval(function(){
-	acc = acc*0.8 + Math.random()*0.2
-	socket.broadcast({x:time, y:acc})
-	time += 0.05
-}, 50)
+
+var inputserver = net.createServer(function (c) {
+  c.setEncoding('utf8')
+  c.write('hello\n');
+  c.on('data', function(d){
+  	lines = d.replace('\r', '').split('\n')
+  	for (var i=0; i<lines.length; i++){
+  		if (!lines[i]) continue;
+  		p = lines[i].split(' ')
+  		console.log(p)
+  		socket.broadcast({x:parseFloat(p[0]), y:parseFloat(p[1])})
+  	}
+  })
+});
+inputserver.listen(8098);
+
