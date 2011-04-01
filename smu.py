02b67ed8c90c869de1470f4507ea2868f7121874
Ian Daniher <it.daniher@gmail.com>
2011-04-01 02:29:47 +0000

    added nice exception handling, intelligent error messages, fixed sign convention

--- smu.py
+++ smu.py
@@ -15,7 +15,7 @@ sock.settimeout(tick)
 def log():
 	t = time.time()
 	data = smu.update()
-	upData = "%.4f %.4f %.4f %s" % (t, data[0], -data[1]*1000, smu.driving)
+	upData = "%.4f %.4f %.4f %s" % (t, data[0], data[1]*1000, smu.driving)
 	sock.send(upData)
 
 while 1:
@@ -24,14 +24,30 @@ while 1:
 		for i in s.split('\n'):
 			i = i.strip()
 			if not i: continue
-			prop, val = i.split()
-			print prop, val
+			try:
+				prop, val = i.split()
+			except Exception as inst:
+				print "exception:", inst
+				print "bad key/value pair:", prop, val
+				prop = 'NONE'
+				val = 0
 			if prop == 'v':
-				smu.set(volts=float(val))
+				try:
+					val = float(val)
+				except Exception as inst:
+					print "exception:", inst
+					print "bad value:", val
+				smu.set(volts=val)
 			elif prop == 'i':
-				smu.set(amps=float(val)/1000.0)
+				try:
+					val = float(val)/1000.0
+				except Exception as inst:
+					print "exception:", inst
+					print "bad value:", val
+				smu.set(amps=val)
+			else:
+				print "bad key"
 	except socket.timeout: pass
-
 	log()
 	
 
