26ec9218af62c6339e068c1ec93870e90e65670e
Ian Daniher <it.daniher@gmail.com>
2011-04-24 14:46:42 -0400

    added driver for RTD/Power Resistor in parallel

---
+++ sensors/RTD.py
@@ -0,0 +1,23 @@
+from __future__ import division
+
+try:
+	from quantities import degC, ohm
+except:
+	degC = ohm = 1
+
+class RTD:
+	def __init__(self):
+		self.A = 3.9083 * 10**-3 * degC**-1
+		self.B = -5.775 * 10**-7 * degC**-2
+		self.Ri = 100 * ohm
+		self.R1 = 100 * ohm
+	def getTemp(self, Req):
+		Rt = Req * self.R1 / (self.R1 - Req)
+		dR = Rt/self.Ri
+		t = (dR-1)/self.A
+		return t
+	def getTarget(self, t):
+		dRTheory = 1 + self.A * t + self.B * t**2
+		R0 = dRTheory * self.Ri
+		Req = (R0 * self.R1)/(R0 + self.R1)
+		return Req
