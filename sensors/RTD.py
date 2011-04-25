9764d394b94e23dcdf80a758facd80734bdb5cb2
Ian Daniher <it.daniher@gmail.com>
2011-04-25 14:42:43 -0400

    added docstrings

--- sensors/RTD.py
+++ sensors/RTD.py
@@ -12,11 +12,13 @@ class RTD:
 		self.Ri = 100 * ohm
 		self.R1 = 100 * ohm
 	def getTemp(self, Req):
+		"""convert equivalent resistance(as seen by the SMU) to temperature"""
 		Rt = Req * self.R1 / (self.R1 - Req)
 		dR = Rt/self.Ri
 		t = (dR-1)/self.A
 		return t
 	def getTarget(self, t):
+		"""get theoretical resistance for a target temperature"""
 		dRTheory = 1 + self.A * t + self.B * t**2
 		R0 = dRTheory * self.Ri
 		Req = (R0 * self.R1)/(R0 + self.R1)
