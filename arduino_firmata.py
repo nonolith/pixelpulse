31f41f0bff3ecc8f0792e0eb176fee87f5c5d4da
Kevin Mehall <km@kevinmehall.net>
2011-07-06 19:35:57 -0600

    Add servo support

--- arduino_firmata.py
+++ arduino_firmata.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# -*- coding: utf8 -*-
 # Arduino Firmata driver for Pixelpulse
 # Distributed under the terms of the BSD License
 # (C) 2011 Kevin Mehall (Nonolith Labs) <km@kevinmehall.net>
@@ -19,10 +20,14 @@ CMD_RESET = 0xff
 SAMPLING_INTERVAL = 0x7A
 CMD_VERSION = 0xF9
 
+PIN_MODE_INPUT = 0
+PIN_MODE_OUTPUT = 1
+PIN_MODE_SERVO = 4
+
 sample_interval_ms = 50
 
 class FirmataDevice(pixelpulse.Device):
-	def __init__(self, port='/dev/ttyUSB0', dpins=[], apins=[]):
+	def __init__(self, port='/dev/ttyUSB0', dpins=[], apins=[], spins=[]):
 		self.analogChannels = {}
 		for n, pin in enumerate(apins):
 			ch = pixelpulse.AnalogChannel('A%i'%pin, 'V', 0.0, 5.0, showGraph=(n<=1))
@@ -40,7 +45,17 @@ class FirmataDevice(pixelpulse.Device):
 			ch.value = 0.0
 			self.digitalChannels[pin] = ch
 			
-		self.channels = self.analogChannels.values() + self.digitalChannels.values()
+		self.servoChannels = {}
+		for n, pin in enumerate(spins):
+			ch = pixelpulse.AnalogChannel('Servo%i'%pin, u'°', 0.0, 180.0, 
+			                              showGraph=(n<=1), 
+			                              state='output', stateOptions=['output'],
+			                              onSet=self.servoSet)
+			ch.pin = pin
+			ch.value = 90.0
+			self.servoChannels[pin] = ch
+			
+		self.channels = self.analogChannels.values() + self.digitalChannels.values() + self.servoChannels.values()
 		self.port = port
 		
 		# store incoming commands until they are complete and can be process
@@ -93,7 +108,8 @@ class FirmataDevice(pixelpulse.Device):
 		
 	def onPoll(self):
 		return   [(ch, ch.value) for ch in self.digitalChannels.values()] \
-		       + [(ch, ch.value) for ch in self.analogChannels.values()]
+		       + [(ch, ch.value) for ch in self.analogChannels.values()] \
+		       + [(ch, ch.value) for ch in self.servoChannels.values()]
 		
 	def setup_report(self):
 		""" Configure Firmata to report values for selected pins """
@@ -109,6 +125,9 @@ class FirmataDevice(pixelpulse.Device):
 			
 		for i in self.digitalChannels.keys():
 			self.setMode(i, False)
+			
+		for pin in self.servoChannels.keys():
+			self.serial.write(chr(SET_PIN_MODE) + chr(pin) + chr(PIN_MODE_SERVO))
 			
 	def setMode(self, pin, isOutput):
 		""" Set a digital pin as an input or output """
@@ -153,38 +172,45 @@ class FirmataDevice(pixelpulse.Device):
 		self.serial.write(chr(DIGITAL_MESSAGE | port) 
 		                + chr(self.digitalOut[port] & 0x7f) 
 		                + chr(self.digitalOut[port] >> 7))
+		                
+	def servoSet(self, channel, val, state):
+		if val is not None:
+			channel.value = min(max(val,0),180)
+			self.serial.write(chr(ANALOG_MESSAGE | channel.pin) + chr(int(channel.value)) + chr(0))
 
-def parse_pin_spec(s, dpins=[], apins=[]):
-        """ Parse pin specifications such as a0 (analog 0), d4 (digital 4), D3, d2-5 """
+def parse_pin_spec(s, dpins=[], apins=[], spins=[]):
+	""" Parse pin specifications such as a0 (analog 0), d4 (digital 4), D3, d2-5 """
 	if s[0].lower() == 'd':
 		l = dpins
-		nmin = 2
-		nmax = 13
+		valid_pins = range(0, 13+1)
 	elif s[0].lower() == 'a':
 		l = apins
-		nmin = 0
-		nmax = 5
+		valid_pins = range(0, 5+1)
+	elif s[0].lower() == 's':
+		l = spins
+		valid_pins = [3,5,6,9,10,11]
+		
 	else:
 		raise ValueError("Invalid pin specification %s"%s)
 	
 	if '-' in s:
-	    p = s[1:].split('-')
-	    if not len(p) == 2:
-	        raise ValueError("Invalid range")
-	    lo = int(p[0])
-	    hi = int(p[1])
-	    if nmin<=lo<= nmax and nmin<=hi<=nmax:
-	        l.extend(range(lo, hi+1))
-	    else:
-	        raise ValueError("Specification %s out of range %i <= n <= %i"%(s, nmin, nmax))
+		p = s[1:].split('-')
+		if not len(p) == 2:
+			raise ValueError("Invalid range")
+	 	lo = int(p[0])
+		hi = int(p[1])
+		for i in range(lo, hi+1):
+	   		if i in valid_pins:
+			 	l.append(i)
+			else:
+				raise ValueError("Pin %i out of range (in %s)"%(i, s))
 	else:
-	    n = int(s[1:])
-	    if nmin <= n <= nmax:
-	        l.append(n)
-	    else:
-	        raise ValueError("Pin %s out of range %i <= n <= %i"%(s, nmin, nmax))
-	        
-	return dpins, apins
+		n = int(s[1:])
+		if n in valid_pins:
+			l.append(n)
+		else:
+			raise ValueError("Pin %s out of range"%(s))
+	return dpins, apins, spins
 	
 		
 if __name__ == '__main__':
@@ -195,14 +221,15 @@ if __name__ == '__main__':
 	
 	apins = []
 	dpins = []
+	spins = []
 	for s in args:
-		parse_pin_spec(s, dpins, apins)
+		parse_pin_spec(s, dpins, apins, spins)
 		
-	if not apins and not dpins:
+	if not apins and not dpins and not spins:
 		print "Using default pins D2-4 D13 A0-3"
 		dpins = range(2, 4+1) + [13]
 		apins = range(3+1)
 	
-	dev = FirmataDevice(port, dpins, apins)
+	dev = FirmataDevice(port, dpins, apins, spins)
 	server = pixelpulse.DataServer(dev)
 	server.start()
