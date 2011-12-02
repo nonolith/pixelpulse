5eedf7e338faa2b040ac4a5376732b15608716f4
Ian Daniher <it.daniher@gmail.com>
2011-12-02 13:14:59 -0600

    updated b12unpack to reflect current packing scheme

--- cee.py
+++ cee.py
@@ -22,7 +22,7 @@ class CEE(object):
 			
 	def b12unpack(self, s):
 		"Turn a 3-byte string containing 2 12-bit values into two ints"
-		return s[0]|((s[1]&0x0f)<<8), ((s[1]&0xf0) >> 4)|(s[2]<<4)
+		return ((((s[2]&0x0f)<<8) | s[0])), ((((s[2]&0xf0)<<4) | s[1]))
 
 	def readADC(self):
 		data = self.dev.ctrl_transfer(0x40|0x80, 0xA0, 0, 0, 6)
